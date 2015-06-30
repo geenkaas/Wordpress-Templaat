21846d3716a4d90be7fa5da17768be18eacd03b2
Geenkaas <github@geenkaas.nl>
2015-06-30 10:55:09 +0200

    Edited 404 page
    Added new structure to the 404 page.

--- geenkaas/404.php
+++ geenkaas/404.php
@@ -1,20 +1,34 @@
-				<?php get_header(); ?>
-				<article id="wrapper404">
-				   <h3>Not found (Error 404)</h3>
-				   <div>
-					   <p>Sorry, but the page you were trying to view does not exist.</p>
-					   <p>It looks like this was the result of either:</p>
-					   <ul>
-						   <li>a mistyped address</li>
-						   <li>an out-of-date link</li>
-					   </ul>
-				   </div>
+<?php get_header(); ?>
 
-					<script>
-					var GOOG_FIXURL_LANG = (navigator.language || '').slice(0,2),
-						GOOG_FIXURL_SITE = location.host;
-					</script>
-					<script src="http://linkhelp.clients.google.com/tbproxy/lh/wm/fixurl.js"></script>
-				</article>
-				<?php get_footer(); ?>
+  <div class="banded">
+    <div class="contentwrapper" role="main">
 
+        <article id="wrapper404" class="postwrapper">
+
+          <header>
+            <h1 class="pageheader" role="heading">
+              Not found (Error 404)
+            </h1>
+          </header>
+
+           <div>
+             <p>Sorry, but the page you were trying to view does not exist.</p>
+             <p>It looks like this was the result of either:</p>
+             <ul>
+               <li>a mistyped address</li>
+               <li>an out-of-date link</li>
+             </ul>
+           </div>
+
+          <script>
+          var GOOG_FIXURL_LANG = (navigator.language || '').slice(0,2),
+            GOOG_FIXURL_SITE = location.host;
+          </script>
+          <script src="http://linkhelp.clients.google.com/tbproxy/lh/wm/fixurl.js"></script>
+
+        </article>
+
+      </div>
+    </div>
+
+<?php get_footer(); ?>
